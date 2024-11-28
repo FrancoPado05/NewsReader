d221df25073637c40c675874914ef2e0bfe49737
FrancoPadovani <fpadovani@frba.utn.edu.ar>
2024-11-27 22:41:20 -0300

    corregi el host en app.py

--- api/app.py
+++ api/app.py
@@ -109,4 +109,5 @@ def comandosDeMySQLEstaEnLaBaseDeDatos(title):
     return True
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
