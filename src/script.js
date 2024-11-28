ec346a420efca921c04b28c7088c18362a490c19
FrancoPadovani <fpadovani@frba.utn.edu.ar>
2024-11-27 22:45:45 -0300

    corregi el port en script.js

--- src/script.js
+++ src/script.js
@@ -51,7 +51,7 @@ form.addEventListener('submit', async (event) => {
     result.textContent = 'Cargando...';
 
     try {
-        const response = await fetch('http://127.0.0.1:5000/scrapear', {
+        const response = await fetch('http://127.0.0.1:10000/scrapear', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
